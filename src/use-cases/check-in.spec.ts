bf716369d7d8dc1977a9d3a745f90b1580789ced
Kellen Pinho <kellen.neves@vanguardatech.com>
2025-12-13 14:48:52 -0300

    feature: create checkin

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test, describe, it, beforeEach } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { AuthenticateUseCase } from "./authenticate";
+import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { CheckInUseCase } from "./check-in";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: CheckInUseCase;
+
+describe("CheckIn Use Case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new CheckInUseCase(checkInsRepository);
+  });
+
+  it("should be able to check in", async () => {
+    const checkIn = await checkInsRepository.create({
+      gym_id: "gym01",
+      user_id: "user01",
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
